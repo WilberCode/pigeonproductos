baaaaeaf3b2ce71cc3ff6706a914f34d4c895bd4
wilber <parionahuaraca@gmail.com>
2023-04-07 09:58:49 -0500

    cambios

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -8,7 +8,21 @@
     <script>document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');</script>
      <meta name="keywords" content="biberón plástico, boca ancha, souftouch, twin pack, triple pack, vidrio flexible, plástico decorado, paladar labio hendido o fisurado, chupete largo, tetinas boca ancha, tetinas flexibles, extractor manual básico, extractor manual premium, extractor de leche eléctrico compacto, extractor eléctrico go mini, biberón de almacenamiento, lactancia, destete, limpieza y esterilización, consuelos y mordedores, cuidado del bebé, salud"> 
     <?php wp_head() ?> 
+    <!-- Global site tag (gtag.js) - Google Analytics -->  
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-199275406-1"></script>
+      <script>
    
+      window.onload = function(){
+          setTimeout(function(){
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-199275406-1'); 
+        
+        },3000) 
+      } 
+    </script>
 </head>
 <body <?php body_class('ready')  ?>  id="top"   > 
  
